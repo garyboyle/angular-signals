d652c043c7c6e067eb674be208f3924a7f680e85
Gary Boyle <gary@shayoo.ie>
2023-05-04 23:55:16 +0100

    Hello signal world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-signals';
+
+  count = signal(0);
+
+  increment() {
+    this.count.set(5);
+  }
+
+  ngOnInit(): void {
+    this.increment();
+  }
 }
